e2b8ec3d9ef8466551ca468291ed3a93270fd7be
kavithaguru321 <82101206+kavithaguru321@users.noreply.github.com>
2021-04-08 06:43:21 +0530

    Update login2.js

--- login2.js
+++ login2.js
@@ -2,7 +2,7 @@ function validate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if (username == "admin" && password == "kavi") {
-        //window.open = "D:/Projectout/videoavi.html";
+        window.open = "index.html";
        // window.location = "file:///D:/Projectout/videoavi.html";
         return true;
      }
